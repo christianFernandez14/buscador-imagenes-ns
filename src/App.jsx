008a6172769259c2f6fc176f4d6f358764a0736e
Christian Fernandez <christian14@gmail.com>
2023-12-23 06:51:00 -0300

    Trabajando con el Header / estructura basica del Form / Algunos Estilos (97)

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,42 @@
+import { Formik, Form, Field } from "formik"
 
-const App = ()=>  {
+import styled from "@emotion/styled"
+
+const Header = styled.header`
+  background-color: #fff;
+  display: flex;
+  justify-content: center;
+  padding: 22px;
+  box-shadow: 1px 2px 5px rgb(0,0,0, .2);
+  position: relative;
+  z-index: 2;
+
+  input{
+    outline: none;
+    border: 1px solid #eee;
+    padding: 10px 5px;
+    border-radius: 5px;
+  }
+`
+
+const App = () => {
 
   return (
     <div>
-      App
+      <Header>
+        <Formik
+          initialValues={{ search: '' }}
+          onSubmit={async values => {
+            // LLamaremos a una API (unplash.com)
+            console.log(values)
+          }}
+        >
+          <Form>
+            <Field name='search' />
+          </Form>
+
+        </Formik >
+      </Header>
     </div>
   )
 }
